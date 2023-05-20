44d4c14fc0b26896e5d6f3c063bbea70d7a9ce98
Bicaru20 <91594426+Bicaru20@users.noreply.github.com>
2023-05-21 01:20:33 +0200

    Model comentat (falta el foward)

--- src/models/conTextTransformer.py
+++ src/models/conTextTransformer.py
@@ -17,30 +17,30 @@ import fasttext.util
 class ConTextTransformer(nn.Module):
     def __init__(self, *, image_size, num_classes, dim, depth, heads, mlp_dim, channels=3):
         super().__init__()
-
-        resnet50 = torchvision.models.resnet50(pretrained=True)
-        modules=list(resnet50.children())[:-2]
-        self.resnet50=nn.Sequential(*modules)
-        for param in self.resnet50.parameters():
+        
+        resnet50 = torchvision.models.resnet50(pretrained=True) #Carregar el model pre-entrenat resnet50
+        modules=list(resnet50.children())[:-2] #Agafar tots els layers menys els dos últims
+        self.resnet50=nn.Sequential(*modules) #Crear un model amb els layers anteriors
+        for param in self.resnet50.parameters(): #Congelar els paràmetres del model
             param.requires_grad = False
-        self.num_cnn_features = 64  # 8x8
-        self.dim_cnn_features = 2048
-        self.dim_fasttext_features = 300
+        self.num_cnn_features = 64  # 8x8 Num característiques de la CNN
+        self.dim_cnn_features = 2048 # 8x8x2048 Dimensió de les característiques de la CNN
+        self.dim_fasttext_features = 300 # Dimensió de les característiques de FastText
 
-        self.pos_embedding = nn.Parameter(torch.randn(1, self.num_cnn_features + 1, dim))
-        self.cnn_feature_to_embedding = nn.Linear(self.dim_cnn_features, dim)
-        self.fasttext_feature_to_embedding = nn.Linear(self.dim_fasttext_features, dim)
-        self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
-        encoder_layer = nn.TransformerEncoderLayer(d_model=dim, nhead=heads, dim_feedforward=mlp_dim, batch_first=True)
-        encoder_norm = nn.LayerNorm(dim)
-        self.transformer = nn.TransformerEncoder(encoder_layer, num_layers=depth)
+        self.pos_embedding = nn.Parameter(torch.randn(1, self.num_cnn_features + 1, dim)) # Embedding de posició. Matriu de incrustació on capturem informació de la posició de les característiques extretes de la CNN
+        self.cnn_feature_to_embedding = nn.Linear(self.dim_cnn_features, dim) # Capa lineal on passem les caracterísitques de la CNN de dimensió 2048 a dimensió dim
+        self.fasttext_feature_to_embedding = nn.Linear(self.dim_fasttext_features, dim) # Capa lineal on passem les caracterísitques de FastText de dimensió 300 a dimensió dim
+        self.cls_token = nn.Parameter(torch.randn(1, 1, dim)) # Parametre entrenable on capturem la informació global del context com les característiques locals de les imatges i els textos d'entrada
+        encoder_layer = nn.TransformerEncoderLayer(d_model=dim, nhead=heads, dim_feedforward=mlp_dim, batch_first=True) # Capa individual del encoder del transformer. Aquesta capa té com a entrada les característiques de la CNN i FastText i com a sortida les característiques de la CNN i FastText amb més informació contextual
+        encoder_norm = nn.LayerNorm(dim) # Normalització de les característiques de la CNN i FastText. LayerNorm redueix la covariancia
+        self.transformer = nn.TransformerEncoder(encoder_layer, num_layers=depth) # Consta de num_layers capes del encoder del transformer. Apila i contecta els blocs de encoder_layer
 
-        self.to_cls_token = nn.Identity()
+        self.to_cls_token = nn.Identity() #Capa identitat que no altera les dades. Posteriorment la utilitzem per agafar la informació del cls_token
 
         self.mlp_head = nn.Sequential(
-            nn.Linear(dim, mlp_dim),
-            nn.GELU(),
-            nn.Linear(mlp_dim, num_classes)
+            nn.Linear(dim, mlp_dim), #Espai dimensional més gran per poder captar millor la informació
+            nn.GELU(), #Funcio d'activacio que introdueix no linearitat i millor la capacitat de captar relacions compelxes entre les dades
+            nn.Linear(mlp_dim, num_classes) #Capa lineal que ens dona la probabilitat de que la img sigui de cada classe
         )
 
     def forward(self, img, txt, mask=None):
